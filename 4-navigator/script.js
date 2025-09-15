ba8058a4a3457c2756f4f54a74e2b6645fa25bb8
Александр <medikov2010@yandex.ru>
2025-09-15 13:57:46 +0300

    Update script.js

--- 4-navigator/script.js
+++ 4-navigator/script.js
@@ -3,5 +3,5 @@ let addressLong = 12;
 let positionLat = 20;
 let positionLong = 25;
 
-let distance = Math.sqrt(Math.pow(positionLat, 2) + Math.pow(positionLong, 2)) + Math.sqrt(Math.pow(addressLat, 2) + Math.pow(addressLong, 2));
+let distance = Math.sqrt(Math.pow(addressLat - positionLat, 2) + Math.pow(addressLong - positionLong, 2));
 console.log(Math.round(distance));
